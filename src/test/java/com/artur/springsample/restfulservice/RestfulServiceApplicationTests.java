3d4ea85475b8a40375d5d0bf4da84b4b9cb00db4
Artur <arturvt@gmail.com>
2018-12-18 10:59:33 -0300

    project base

---
+++ src/test/java/com/artur/springsample/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.artur.springsample.restfulservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
